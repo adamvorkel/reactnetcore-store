aba401024e5f97f0fa38f41eed9d3fff097a0ca8
Adam Vorkel <adamvorkel@gmail.com>
2020-09-29 21:33:04 +0200

    products controller GET endpoint created, returns array of products

--- api/Controllers/ProductsController.cs
+++ api/Controllers/ProductsController.cs
@@ -6,16 +6,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
+using api.Models;
+
 namespace api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ProductsController
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
         {
-            return "OK!";
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.ToArray();
         }
     }
 }
